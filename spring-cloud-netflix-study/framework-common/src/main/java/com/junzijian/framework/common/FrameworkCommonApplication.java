b9729a2cae4ed171e20c29e6f71bdb87be412134
liuzhe <718161567@qq.com>
2019-03-30 16:58:15 +0800

    base project create

---
+++ spring-cloud-netflix-study/framework-common/src/main/java/com/junzijian/framework/common/FrameworkCommonApplication.java
@@ -0,0 +1,13 @@
+package com.junzijian.framework.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkCommonApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-netflix-study/service-user-center-oauth/src/main/java/com/junzijian/user/center/oauth/UserCenterOauthApplication.java
@@ -0,0 +1,13 @@
+package com.junzijian.user.center.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCenterOauthApplication.class, args);
+	}
+
+}
