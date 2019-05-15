3bd01ffdc8a8fd8dc939469ef4b36cc7e190aadb
liuzhe <718161567@qq.com>
2019-05-15 16:42:37 +0800

    项目结构整理 & config-server

---
+++ spring-cloud-netflix-study/govern/govern-zipkin-server/src/test/java/com/junzijian/govern/zipkin/server/ZipKinServerAppTests.java
@@ -0,0 +1,20 @@
+package com.junzijian.govern.zipkin.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author liuzhe
+ * @date 2019/5/15
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipKinServerAppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
