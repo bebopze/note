60f8d732422318150ebb37f2eb13b2544404d8b0
liuzhe <718161567@qq.com>
2019-06-23 02:35:46 +0800

    cloud-alibaba

---
+++ spring-cloud-alibaba-study/govern/govern-gateway/govern-gateway-sc-gateway/src/main/java/com/junzijian/cloud/govern/gateway/sc/gateway/GatewayApp.java
@@ -0,0 +1,16 @@
+package com.junzijian.cloud.govern.gateway.sc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuzhe
+ * @date 2019/3/30
+ */
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
