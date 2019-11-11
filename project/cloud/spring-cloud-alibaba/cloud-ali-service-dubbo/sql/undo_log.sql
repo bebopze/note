e438d61c18649b5e5ef07bb62c425b1b429f99fb
liuzhe <718161567@qq.com>
2019-11-11 21:17:14 +0800

    seata undo_log

---
+++ project/cloud/spring-cloud-alibaba/cloud-ali-service-dubbo/sql/undo_log.sql
@@ -0,0 +1,26 @@
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for undo_log
+-- 注意此处0.3.0+ 增加唯一索引 ux_undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of undo_log
+-- ----------------------------
+SET FOREIGN_KEY_CHECKS=1;
