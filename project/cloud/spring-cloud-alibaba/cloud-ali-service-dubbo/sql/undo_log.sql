f554c709a4a23cd47601c0240af2bb1401d77359
liuzhe <718161567@qq.com>
2019-11-14 20:23:27 +0800

    init business sql

--- project/cloud/spring-cloud-alibaba/cloud-ali-service-dubbo/sql/undo_log.sql
+++ project/cloud/spring-cloud-alibaba/cloud-ali-service-dubbo/sql/undo_log.sql
@@ -1,6 +1,47 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 
+
+CREATE TABLE `storage` (
+  `id` bigint(20) unsigned NOT NULL COMMENT 'ID',
+  `product_id` bigint(20) unsigned NOT NULL COMMENT '商品ID',
+  `product_name` varchar(32) DEFAULT NULL COMMENT '商品name',
+  `inventory` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '库存数量',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '状态 (1:正常；2-锁定；)',
+  `version` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁版本号',
+  `gmt_create` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modify` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品库存表';
+
+
+CREATE TABLE `account` (
+  `id` bigint(20) unsigned NOT NULL COMMENT '账户ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  `amount` decimal(12,2) unsigned NOT NULL COMMENT '账户金额',
+  `version` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁版本号',
+  `gmt_create` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modify` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='账户表';
+
+
+CREATE TABLE `tb_order` (
+  `id` bigint(20) unsigned NOT NULL COMMENT '订单ID',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '用户ID',
+  `product_id` bigint(20) unsigned NOT NULL COMMENT '商品ID',
+  `product_name` varchar(64) DEFAULT NULL COMMENT '商品name',
+  `product_price` decimal(12,2) unsigned NOT NULL COMMENT '商品单价',
+  `product_num` int(11) unsigned NOT NULL COMMENT '商品数量',
+  `product_total_price` decimal(12,2) unsigned NOT NULL COMMENT '商品总价',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '订单状态 (1-待付款;2-已付款;3-主动弃单;4-支付超时弃单;)',
+  `version` bigint(20) unsigned NOT NULL DEFAULT '1' COMMENT '乐观锁版本号',
+  `gmt_create` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modify` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+
 -- ----------------------------
 -- Table structure for undo_log
 -- 注意此处0.3.0+ 增加唯一索引 ux_undo_log
