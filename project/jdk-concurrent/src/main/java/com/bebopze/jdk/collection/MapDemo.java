c9c592ae27ed5de2110325f40433a72cb9a93c12
liuzhe <718161567@qq.com>
2020-06-30 12:13:15 +0800

    HashMap源码分析

--- project/jdk-concurrent/src/main/java/com/bebopze/jdk/collection/MapDemo.java
+++ project/jdk-concurrent/src/main/java/com/bebopze/jdk/collection/MapDemo.java
@@ -15,7 +15,38 @@ public class MapDemo {
 
 //        test1();
 
-        test2_ConcurrentHashMap();
+        test_HashMap();
+
+//        test2_ConcurrentHashMap();
+    }
+
+
+    private static HashMap<Integer, String> map = new HashMap(2, 0.75f);
+
+    /**
+     * HashMap Infinite Loop
+     */
+    private static void test_HashMap() {
+
+        map.put(5, "C");
+
+        new Thread("Thread1") {
+
+            @Override
+            public void run() {
+                map.put(7, "B");
+                System.out.println(map);
+            }
+        }.start();
+
+        new Thread("Thread2") {
+
+            @Override
+            public void run() {
+                map.put(3, "A");
+                System.out.println(map);
+            }
+        }.start();
     }
 
 
