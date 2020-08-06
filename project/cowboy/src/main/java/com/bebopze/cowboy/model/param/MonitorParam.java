1f25673ea27b4832aec7e1e80e18c4a6e7a80216
liuzhe <718161567@qq.com>
2020-08-06 13:06:57 +0800

    cowboy stock market

---
+++ project/cowboy/src/main/java/com/bebopze/cowboy/model/param/MonitorParam.java
@@ -0,0 +1,13 @@
+package com.bebopze.cowboy.model.param;
+
+import lombok.Data;
+
+/**
+ * @author bebopze
+ * @date 2020/7/24
+ */
+@Data
+public class MonitorParam {
+
+    private String code;
+}
---
+++ project/cowboy/src/main/java/com/bebopze/cowboy/service/impl/StockMarketServiceImpl.java
@@ -0,0 +1,24 @@
+package com.bebopze.cowboy.service.impl;
+
+import com.bebopze.cowboy.model.param.MonitorParam;
+import com.bebopze.cowboy.service.StockMarketService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author bebopze
+ * @date 2020/7/24
+ */
+@Service
+public class StockMarketServiceImpl implements StockMarketService {
+
+
+    @Override
+    public Object monitorStockPrice(MonitorParam param) {
+        return null;
+    }
+
+    @Override
+    public Object monitorNorthwardFund(MonitorParam param) {
+        return null;
+    }
+}
