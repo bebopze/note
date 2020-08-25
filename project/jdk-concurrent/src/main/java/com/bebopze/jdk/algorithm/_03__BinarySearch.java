af621360bfc60ac74722fa46ceb6cac7b6671e58
liuzhe <718161567@qq.com>
2020-08-25 15:28:56 +0800

    二分查找

--- project/jdk-concurrent/src/main/java/com/bebopze/jdk/algorithm/_03__BinarySearch.java
+++ project/jdk-concurrent/src/main/java/com/bebopze/jdk/algorithm/_03__BinarySearch.java
@@ -11,7 +11,11 @@ public class _03__BinarySearch {
 
     public static void main(String[] args) {
 
+        // 简易
         test__1();
+
+        // 变体
+        test__2();
     }
 
 
@@ -28,7 +32,17 @@ public class _03__BinarySearch {
     }
 
 
-    // ------------------------- 二分查找 实现 ----------------------------
+    private static void test__2() {
+
+        int[] arr = new int[]{1, 5, 9, 13, 17, 17, 17, 66, 88, 88, 99};
+
+        System.out.println("查找第一个值等于给定值的元素     ------- " + bsearch_1(arr, 17));
+        System.out.println("查找最后一个值等于给定值的元素    ------- " + bsearch_2(arr, 17));
+        System.out.println("查找第一个大于等于给定值的元素    ------- " + bsearch_3(arr, 18));
+        System.out.println("查找最后一个小于等于给定值的元素  ------- " + bsearch_4(arr, 17));
+    }
+
+    // ------------------------- 二分查找 给定值查找 实现 ----------------------------
 
     /**
      * 1、循环实现
@@ -82,5 +96,101 @@ public class _03__BinarySearch {
         }
     }
 
+
+    // ----------------------------- 二分查找 变体 实现 -------------------------------
+
+
+    /**
+     * 1、查找第一个值等于给定值的元素
+     *
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearch_1(int[] a, int value) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == 0) || (a[mid - 1] != value)) return mid;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 2、查找最后一个值等于给定值的元素
+     *
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearch_2(int[] a, int value) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == a.length - 1) || (a[mid + 1] != value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 3、查找第一个大于等于给定值的元素
+     *
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearch_3(int[] a, int value) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 4、查找最后一个小于等于给定值的元素
+     *
+     * @param a
+     * @param value
+     * @return
+     */
+    public static int bsearch_4(int[] a, int value) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == a.length - 1) || (a[mid + 1] > value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
 
 }
