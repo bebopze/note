f370af3a04b4336f387f3c1a67890d25f388b604
liuzhe <718161567@qq.com>
2020-12-16 22:36:01 +0800

    字符串常量池（String Pool）-> "缓存"     // "享元模式"  ->  不变 + 共享  ==>  节省内存
        String两种赋值方式：
    
            1、引用类型 赋值方式                         // 堆     ->     新建
    
                String str_1 = new String("abc");
    
                    1、在堆中 创建String对象，在构造函数中 -> 传入value字段的值 -> "abc"    返回String对象的堆地址引用
                    2、将 堆地址引用  赋值给  变量str_1
    
            2、“基本类型” 赋值方式                       // string-pool（字符串常量池）  ->  缓存
    
                String s_1 = "abc";
    
                    1、在 string-pool（字符串常量池） 中"查缓存"   ==>   有 -> 返回引用   /  无 -> 创建"abc"对象，返回引用
                    2、将 常量池中的引用  -->  赋值给 变量s_1
    
        --------------------------------------------------------------------------------------------------------------------
        str_1.intern();                 // 堆 -> 字符串缓冲池
    
            1、在 string-pool（字符串常量池） 中"查缓存"
                有 -> 返回 常量池引用
                无 -> 将 堆中对象str_1  copy一份到  string-pool（字符串常量池）

--- project/jdk-concurrent/src/main/java/com/bebopze/jdk/string/StringDemo.java
+++ project/jdk-concurrent/src/main/java/com/bebopze/jdk/string/StringDemo.java
@@ -1,6 +1,8 @@
 package com.bebopze.jdk.string;
 
 /**
+ * - String、String Pool（字符串常量池）                               - https://www.cnblogs.com/fangfuhai/p/5500065.html
+ *
  * @author bebopze
  * @date 2020/6/12
  */
@@ -8,7 +10,83 @@ public class StringDemo {
 
     public static void main(String[] args) {
 
+        test_1();
+
+//        test_2();
+    }
+
+
+    private static void test_1() {
+
+        // ================ 两种赋值方式 ================
+
+        // --------------------------------------- 1、引用类型  赋值方式 ---------------------------------------
+        // -
+        // -   1、在堆中 创建String对象，在构造函数中 -> 传入value字段的值 -> "abc"    返回String对象的堆地址引用
+        // -
+        // -   2、将 堆地址引用  赋值给  变量str_1
+
+
+        String str_1 = new String("abc");
+        String str_2 = new String("abc");
+
+
+        // --------- intern
+        // -
+        // -   1、在 string-pool（字符串常量池） 中"查缓存"   ==>   有 -> 返回 常量池引用
+        // -
+        // -   2、无 -> 将 堆中对象  copy一份到  string-pool（字符串常量池）
+
+        String str_intern = str_1.intern();
+
+
+        // --------------------------------------- 2、“基本类似” 赋值方式 ---------------------------------------
+        // -
+        // -   1、在 string-pool（字符串常量池） 中"查缓存"   ==>   有 -> 返回引用   /  无 -> 创建"abc"对象，返回引用
+        // -
+        // -   2、将 常量池中的引用  -->  赋值给 变量s_1
+
+        String s_1 = "abc";
+        String s_2 = "abc";
+
+
+        // ---------------------------------------------------------------------
+
+        System.out.println("=========================================");
+
+
+        System.out.println("str_1 == str_2   ->   " + (str_1 == str_2));
+        System.out.println("  s_1 == s_2     ->   " + (s_1 == s_2));
+
+
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("str_1 == s_1   ->   " + (str_1 == s_1));
+
+
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("str_intern == str_1   ->   " + (str_intern == str_1));
+
+
+        System.out.println("-----------------------------------------");
+
+
+        System.out.println("str_intern == s_1   ->   " + (str_intern == s_1));
+        System.out.println("str_intern == s_2   ->   " + (str_intern == s_2));
+
+
+        System.out.println("=========================================");
+    }
+
+
+    private static void test_2() {
+
+
 //        InputStream in = System.in;
+
 
         String s1 = "";
         s1 = "String常量池";
